c5ee7d2db0054d2b51f8adf576d19ae6e4657c4d
Rémi Jarjat <dreeckan@gmail.com>
2021-04-14 10:31:26 +0200

    cours Jquery - Partie 1

---
+++ JQuery/cours.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let links = $('a');
+console.log(links);
+
+let footers = $('#footer');
+console.log(footers);
+
+let articles = $('.article');
+console.log(articles);
+
+let p = $("article p:nth-child(2)");
+console.log(p);
