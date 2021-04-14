a36abfe2b21c58d79ffacb5d0763cbeb41f310c3
Rémi Jarjat <dreeckan@gmail.com>
2021-04-14 11:26:01 +0200

    Correction exercices du cours (event)

--- JQuery/cours.js
+++ JQuery/cours.js
@@ -1,13 +1,35 @@
 "use strict";
 
 let links = $('a');
-console.log(links);
+// console.log(links);
 
 let footers = $('#footer');
-console.log(footers);
+// console.log(footers);
 
 let articles = $('.article');
-console.log(articles);
+// console.log(articles);
 
-let p = $("article p:nth-child(2)");
-console.log(p);
+let paragraphs = $("article p:nth-child(2)");
+// console.log(p);
+
+links.on('click', function (event) {
+    event.preventDefault();
+
+    // return false; // équivalent au preventDefault, mais uniquement en JQuery
+});
+
+paragraphs.hover(event => {
+    console.log('Vous entrez');
+}, event => {
+    console.log('Vous sortez');
+});
+
+// Equivalent avec 2 événements
+//
+// paragraphs.on('mouseenter', function () {
+//     console.log('Vous entrez');
+// });
+//
+// paragraphs.on('mouseleave', function () {
+//     console.log('vous sortez');
+// })
